f48a208cda17b141eada866fb8c228226bc7ee80
Jesse Tane <jesse.tane@gmail.com>
2013-03-12 04:00:43 -0400

    refactor

--- mkapp.sh
+++ mkapp.sh
@@ -3,70 +3,66 @@
 # mkapp.sh - scaffold up a new app for bitnami
 #
 
-BITNAMI_ROOT="/opt/bitnami"
-
 mkapp() {
   
+  # settings
+  local STACK="/opt/bitnami"
+  
   # vars
-  name="$1"
-  aliases=("$@")
-  dir="$BITNAMI_ROOT"/apps/"$name"
+  local app="$1"
+  local names=("$@")
+  local home="$STACK"/apps/"$app"
   
   # require app name
-  [ -z "$name" ] && echo "please specify an app name" >&2 && return 1
+  [ -z "$app" ] && echo "please specify an app name" >&2 && return 1
   
-  # ensure app doesn't exist
-  [ -d "$dir" ] && echo "cannot create $name: app exists" >&2 && return 1
+  # ensure app dir doesn't exist
+  [ -d "$home" ] && echo "cannot create $app: app exists" >&2 && return 1
   
-  # build the requisite dirs
+  # do stuff
   __mkapp_create_dirs || return 1
-  
-  # create an apache conf file for our new app
-  __mkapp_create_apache_conf || return 1
-  
-  # edit apache's config file to include our app
-  __mkapp_include_apache_conf || return 1
-  
-  # reload apache with the new app's config
+  __mkapp_create_vhosts || return 1
+  __mkapp_link_vhosts || return 1
+  __mkapp_chown_app || return 1
   __mkapp_reload_apache || return 1
 }
 
 __mkapp_create_dirs() {
-  mkdir -p "$dir"/conf
-  mkdir -p "$dir"/htdocs
+  mkdir -p "$home"/conf
+  mkdir -p "$home"/htdocs
 }
 
-__mkapp_create_apache_conf() {
-  for alias in "${aliases[@]}"
+__mkapp_create_vhosts() {
+  for name in "${names[@]}"
   do
     echo "<VirtualHost *:*>
-  DocumentRoot $dir/htdocs
-  ServerName $alias
-  <Directory $dir/htdocs>
+  DocumentRoot $home/htdocs
+  ServerName $app
+  <Directory $home/htdocs>
     Options +FollowSymLinks
     <IfVersion < 2.3 >
-    Order allow,deny
-    Allow from all
+      Order allow,deny
+      Allow from all
     </IfVersion>
     <IfVersion >= 2.3>
-    Require all granted
+      Require all granted
     </IfVersion>
   </Directory>
-</VirtualHost>" >> "$dir"/conf/"$name".conf
+</VirtualHost>" >> "$home"/conf/httpd.conf
   done
 }
 
-__mkapp_include_apache_conf() {
-  echo -en "\nInclude \"$dir/conf/$name.conf\"" >> "$BITNAMI_ROOT"/apache2/conf/httpd.conf
+__mkapp_link_vhosts() {
+  echo -en "\nInclude \"$home/conf/httpd.conf\"" >> "$STACK"/apache2/conf/httpd.conf
+}
+
+__mkapp_chown_app() {
+  chown -R bitnami:bitnami "$home"
 }
 
 __mkapp_reload_apache() {
-  "$BITNAMI_ROOT"/ctlscript.sh restart apache
+  "$STACK"/ctlscript.sh restart apache
 }
 
-__mkapp_chown_dir() {
-  chown -R bitnami:bitnami "$dir"
-}
-
-# main
-mkapp "$@"
+# if main
+[ "$0" = "$BASH_SOURCE" ] && mkapp "$@"
