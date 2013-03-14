85c7690924a20897066e954b9f4c9e41dfff0116
Jesse Tane <jesse.tane@gmail.com>
2013-03-14 04:48:18 -0400

    bug fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,15 +12,19 @@ lib="$home/lib"
 src="$home/src"
 
 # scaffold
+echo "-- scaffolding directories --"
 mkdir -p "$bin"
 mkdir -p "$lib"
 mkdir -p "$src"
 
 # system packages
+echo "-- installing system packages --"
+sudo apt-get update
 sudo apt-get install git -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install postfix -y
 
 # custom packages
+echo "-- installing custom packages --"
 cd "$src"
 git clone https://github.com/jessetane/argue
 git clone https://github.com/jessetane/bitnami-mkapp
@@ -28,6 +32,7 @@ git clone https://github.com/jessetane/ubuntu-email
 git clone https://github.com/jessetane/httpcat
 
 # generate builds
+echo "-- building custom packages --"
 cd "$lib"
 mkdir argue         && ln -s ../src/argue ./argue/0.0.1
 mkdir bitnami-mkapp && ln -s ../src/bitnami-mkapp ./bitnami-mkapp/current
@@ -35,6 +40,23 @@ mkdir ubuntu-email  && ln -s ../src/ubuntu-email ./ubuntu-email/current
 mkdir httpcat       && ln -s ../src/httpcat ./httpcat/current
 
 # link executables
-echo "echo $lib/bitnami-mkapp/current/bin/mkapp" '"$@"' > "$bin"/mkapp
-echo "echo $lib/ubuntu-email/current/bin/email" '"$@"' > "$bin"/email
-echo "echo $lib/httpcat/current/bin/httpcat" '"$@"' > "$bin"/httpcat
+echo "-- linking custom executables --"
+echo "echo $lib/bitnami-mkapp/current/bin/mkapp" '"$@"' > "$bin"/mkapp && chmod +x "$bin/"mkapp
+echo "echo $lib/ubuntu-email/current/bin/email" '"$@"' > "$bin"/email && chmod +x "$bin/"email
+echo "echo $lib/httpcat/current/bin/httpcat" '"$@"' > "$bin"/httpcat && chmod +x "$bin/"httpcat
+
+# bash profile
+echo "-- configuring bash profile --"
+cd
+
+# these should work even for non-interactive shells
+echo 'export PATH="$bin:$PATH"' >> temp
+echo 'function l { ls -alhBi --group-directories --color "$@"; }' >> temp
+cat .bashrc >> temp
+cat temp > .bashrc
+rm temp
+
+# these are interactive only
+PS1='\u@\h:\w\$(git branch 2> /dev/null | grep -e '\''\* '\'' | sed '\''s/^..\(.*\)/ {\1}/'\'')\$ '
+echo "export PS1=\"$PS1\"" >> .bashrc
+echo "unalias l" >> .bashrc
